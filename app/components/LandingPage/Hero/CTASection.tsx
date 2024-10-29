82edac040a732cc02e2d14f1d7109d8b966315db
Attique Zaheer <mattiquezaheer@gmail.com>
2024-10-29 18:14:49 +0500

    CTA Title size Fix

--- app/components/LandingPage/Hero/CTASection.tsx
+++ app/components/LandingPage/Hero/CTASection.tsx
@@ -9,7 +9,7 @@ export default function AxiomCTASection() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                     {/* Left Div */}
                     <div>
-                        <h2 className="text-[44px] font-semibold text-gray-900 mb-10 leading-[60px]">Discover Axiom: Your Gateway To Custom Software Solutions. Contact Us Today.</h2>
+                        <h2 className=" text-4xl md:text-[44px] md:leading-[60px] font-semibold text-gray-900 mb-10 ">Discover Axiom: Your Gateway To Custom Software Solutions. Contact Us Today.</h2>
                         <p className="text-lg leading-[30px] text-[#454545] mb-10">Experience the innovation of Axiom, where ideas become reality. Our custom software solutions are tailored to your needs, driving efficiency and success. Ready to transform your business? Contact us now.</p>
                         <Link href="/contact" className="bg-blue-500 text-white font-semibold py-4 px-8 rounded-md hover:bg-black hover:text-white transition-colors duration-300">
                             Book a Call
