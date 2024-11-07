8c10cb8725df099236fa15d6cece88286bc78d5f
Attique Zaheer <mattiquezaheer@gmail.com>
2024-11-07 14:39:06 +0500

    Update constants.tsx

--- app/components/constants/constants.tsx
+++ app/components/constants/constants.tsx
@@ -1,6 +1,7 @@
 // export const backendUrl = "http://localhost:5000";
 // Ateeq's backendUrl
-export const backendUrl = "https://xc1t5tk1-5000.inc1.devtunnels.ms";
+export const backendUrl = "https://axiom-backend-two.vercel.app";
+// export const backendUrl = "https://xc1t5tk1-5000.inc1.devtunnels.ms";
 export const websiteUrl = "https://axiom-new.vercel.app";
 // Hammad backendUrl
 // export const backendUrl = "https://8rgvpdmw-5000.euw.devtunnels.ms";
@@ -19,3 +20,6 @@ export const socialMedia = {
     address: "1234 Elm Street Springfield, IL 62704 USA",
     map: "https://www.google.com/maps?ll=33.660129,73.076506&z=15&t=m&hl=en&gl=US&mapclient=embed&cid=13267731845085379525"
 };
+
+
+// eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNzMwOTY2MjIxLCJqdGkiOiJkNjU0ZGQ4Ni0zZGE5LTQ1YWEtOGMyZS04MDk5MDViYzMyNWYiLCJ1c2VyX3V1aWQiOiJlNTIxYjc1MS1hZGIwLTQwMDQtODYzZS02OTNkY2QxYWUxZGEifQ.W8qN8vnFyir-m-senAmK4FvO9fboER7BKoe_qIeH3FaVp7o1PiceGtrhubFGEXb-2ZkzHh4--otHIKzRPfStXQ
