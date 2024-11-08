50c240214c2db76a8316ef5ed98ac048125e4033
Attique Zaheer <mattiquezaheer@gmail.com>
2024-11-08 14:47:13 +0500

    services landing page

--- app/components/Services/Services.tsx
+++ app/components/Services/Services.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { ChevronRight } from "lucide-react";
+import axios from "axios";
 import digital from "@/public/services/digital.svg";
 import DevOps from "@/public/services/DevOps.svg";
 import mobile from "@/public/services/mobile.svg";
@@ -9,82 +10,106 @@ import ssl from "@/public/services/ssl.svg";
 import Cloud from "@/public/services/cloud.svg";
 import avator from "@/public/services/avator.svg";
 import Bussiness from "@/public/services/Business.svg";
+import  {backendUrl} from "../constants/constants";
 
-export default function Services() {
-    const services = [
-        {
-            title: "Digital Solutions",
-            id: "web",
-            link: "/services/web-development",
-            description: "Digital Consulting and Strategy: Tailored guidance and planning to maximize your digital presence.",
-            image: digital,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "DevOps Solutions",
-            id: "web",
-            link: "/services/web-development",
-            description: "Streamlining development processes, enhancing collaboratio.",
-            image: DevOps,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Mobile App Development",
-            id: "web",
-            link: "/services/web-development",
-            description: "Crafting engaging and user-friendly mobile applications seamlessly across platforms.",
-            image: mobile,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Game Development (AR & VR)",
-            id: "web",
-            link: "/services/web-development",
-            description: "Immersive and captivating game development leveraging augmented reality and virtual reality technologies.",
-            image: game,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Web Development",
-            id: "web",
-            link: "/services/web-development",
-            description: "Custom Web Development: Building responsive, intuitive websites tailored to your unique.",
-            image: cms,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Cybersecurity Service",
-            id: "web",
-            link: "/services/web-development",
-            description: "Custom Web Development: Building responsive, intuitive websites tailored to your unique.",
-            image: ssl,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Cloud Services",
-            id: "web",
-            link: "/services/web-development",
-            description: "Cloud Operations and Migration: Smooth transition and efficient management of cloud.",
-            image: Cloud,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Staff Augmentation",
-            id: "web",
-            link: "/services/web-development",
-            description: "Staff Augmentation: Augmenting your workforce with skilled professionals to meet evolving.",
-            image: avator,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-        {
-            title: "Custom Enterprise Solutions",
-            id: "web",
-            link: "/services/web-development",
-            description: "Developing scalable and robust applications tailored to your business needs.",
-            image: Bussiness,
-            // icon: <Code className="h-10 w-10 text-primary" />,
-        },
-    ];
+interface Service {
+    title: string;
+    shortDescription: string;
+    description: string;
+    homeImage: string;
+    coverImage: string;
+    link?: string;
+}
+export default async function Services() {
+    let services: Service[] | null = null;
+    try {
+        const response = await axios.get(`${backendUrl}/api/v1/service`);
+        if (response.data.data) {
+            services = response.data.data;
+        } else {
+            console.error("Failed to fetch Services data:", response.statusText);
+        }
+    } catch (error) {
+        console.log("Error fetching Services Data:", error);
+    }
+
+    if (!services) {
+        return <div className="text-center text-white text-2xl py-10 bg-red-300">Loading...</div>;
+    }
+    // const services = [
+    //     {
+    //         title: "Digital Solutions",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Digital Consulting and Strategy: Tailored guidance and planning to maximize your digital presence.",
+    //         image: digital,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "DevOps Solutions",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Streamlining development processes, enhancing collaboratio.",
+    //         image: DevOps,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Mobile App Development",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Crafting engaging and user-friendly mobile applications seamlessly across platforms.",
+    //         image: mobile,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Game Development (AR & VR)",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Immersive and captivating game development leveraging augmented reality and virtual reality technologies.",
+    //         image: game,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Web Development",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Custom Web Development: Building responsive, intuitive websites tailored to your unique.",
+    //         image: cms,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Cybersecurity Service",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Custom Web Development: Building responsive, intuitive websites tailored to your unique.",
+    //         image: ssl,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Cloud Services",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Cloud Operations and Migration: Smooth transition and efficient management of cloud.",
+    //         image: Cloud,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Staff Augmentation",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Staff Augmentation: Augmenting your workforce with skilled professionals to meet evolving.",
+    //         image: avator,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    //     {
+    //         title: "Custom Enterprise Solutions",
+    //         id: "web",
+    //         link: "/services/web-development",
+    //         description: "Developing scalable and robust applications tailored to your business needs.",
+    //         image: Bussiness,
+    //         // icon: <Code className="h-10 w-10 text-primary" />,
+    //     },
+    // ];
 
     return (
         <section className="service-gradient">
@@ -103,15 +128,16 @@ export default function Services() {
                                 ${[2, 3, 8].includes(index) ? "lg:col-span-2" : ""} 
                                 ${index === 5 ? "lg:col-span-1" : ""}`}>
                                 <h4 className="text-2xl font-bold">{service.title}</h4>
-                                <p className="text-[var(--Gray-Color)] text-base mt-7">{service.description}</p>
+                                <p className="text-[var(--Gray-Color)] text-base mt-7">{service.shortDescription}</p>
 
                                 {/* Image container with fixed height */}
                                 {/* <div className="absolute bottom-6 right-6 max-w-[calc(100%-3rem)] h-[180px] w-[275px]">
                                     <Image src={service.image} alt={service.title} fill={true} className="object-contain w-full h-full" priority={index < 4} quality={80} />
                                 </div> */}
-                                <div className="absolute  bottom-6 right-6 pl-6">
+                                <div className="absolute  bottom-6 right-6 ">
                                     {/* <Image src={service.image} alt={service.title} className="absolute object-contain bottom-6 right-6 pl-12 sm:pl-0" /> */}
-                                    <Image src={service.image} alt={service.title} className=" "   />
+                                    {/* <Image src={service.coverImage} alt={service.title} className=" "   /> */}
+                                   <Image src={service.coverImage} alt={service.title} width={300} height={300} className="" layout="responsive" objectFit="contain" />
                                 </div>
 
                                 {[2, 3, 8].includes(index) && (
