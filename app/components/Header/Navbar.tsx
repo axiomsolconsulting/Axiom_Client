debe2efdfcd52e58fa59a48ecdc39c0e4ccf1f5c
Attique Zaheer <mattiquezaheer@gmail.com>
2024-11-13 10:56:34 +0500

    fix navbar as well

--- app/components/Header/Navbar.tsx
+++ app/components/Header/Navbar.tsx
@@ -5,20 +5,19 @@ import Image from "next/image";
 import { Menu, X } from "lucide-react";
 import { usePathname } from "next/navigation"; // Import usePathname
 
-
 const Navbar = () => {
     const navRef = useRef<HTMLDivElement>(null);
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    
+
     const pathname = usePathname(); // Get current route
-    
+
     // Define allowed routes, including the '/services' base route
     const allowedRoutes = [
-        "/services", 
-        "/about", 
-        "/careers", 
-        "/insights", 
-        "/", 
+        "/services",
+        "/about",
+        "/careers",
+        "/insights",
+        "/",
         "/contact",
         "/",
         // '/privacy-policy',
@@ -33,7 +32,7 @@ const Navbar = () => {
         { name: "About", link: "/about" },
         { name: "Careers", link: "/careers" },
         { name: "Insights", link: "/insights" },
-        { name: "FAQs", link: "/faq" }
+        { name: "FAQs", link: "/faq" },
     ];
 
     const handleClickOutside = (event: MouseEvent) => {
@@ -55,16 +54,9 @@ const Navbar = () => {
             <nav className="absolute top-0 left-0 right-0 bg-transparent text-white z-10">
                 <div className="flex items-center justify-between container px-5 mx-auto h-20">
                     <Link href="/" className="text-2xl font-bold">
-                        <Image
-                            src={Light 
-                                ? "https://res.cloudinary.com/ddmanxpsb/image/upload/v1731071967/Group_1018_1_aee90q.png" 
-                                : "https://res.cloudinary.com/ddmanxpsb/image/upload/v1731071968/Group_1018_1_1_trvhrh.png"}
-                            width={155} 
-                            height={38} 
-                            alt="Axiom Logo"
-                        />
+                        <Image src={Light ? "https://res.cloudinary.com/ddmanxpsb/image/upload/v1731071967/Group_1018_1_aee90q.png" : "https://res.cloudinary.com/ddmanxpsb/image/upload/v1731071968/Group_1018_1_1_trvhrh.png"} width={155} height={38} alt="Axiom Logo" />
                     </Link>
-                    <ul className={`lg:flex items-center justify-between gap-x-7 hidden text-lg font-medium ${Light ? 'text-white' : 'text-black'}`}>
+                    <ul className={`lg:flex items-center justify-between gap-x-7 hidden text-lg font-medium ${Light ? "text-white" : "text-black"}`}>
                         {Navbar.map((item, index) => (
                             <li key={index}>
                                 <Link href={item.link} className={`hover:text-gray-300 py-2 ${pathname === item.link ? "border-b-2 border-[#1C85FF]" : ""}`}>
@@ -75,14 +67,11 @@ const Navbar = () => {
                     </ul>
                     <div className="flex items-center space-x-4">
                         <div className="md:flex items-center space-x-4 hidden">
-                            <Link href="/contact" className={`py-2 px-6 text-lg font-medium ${Light ? 'animated-border bg-transparent' : ' bg-[var(--Blue-Color)] rounded-[6px] hover:bg-[#011633] duration-300'}`}>
+                            <Link href="/contact" className={`py-2 px-6 text-lg font-medium ${Light ? "animated-border bg-transparent" : " bg-[var(--Blue-Color)] rounded-[6px] hover:bg-[#011633] duration-300"}`}>
                                 Book a Call
                             </Link>
                         </div>
-                        <button
-                            aria-label="Mobile Menu"
-                            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                            className={`lg:hidden transition duration-300 ${Light ? 'text-white' : 'text-black'}`}>
+                        <button aria-label="Mobile Menu" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className={`lg:hidden transition duration-300 ${Light ? "text-white" : "text-black"}`}>
                             {isMobileMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
                         </button>
                     </div>
@@ -90,26 +79,26 @@ const Navbar = () => {
             </nav>
 
             {/* Mobile Navbar */}
-            <div ref={navRef} className={`lg:hidden bg-gradient absolute top-16 left-0 right-0 overflow-hidden transition-all duration-300 ease-in-out z-20 ${isMobileMenuOpen ? "max-h-fit opacity-100 visible" : "max-h-0 opacity-0 invisible"}`}>
-                <nav className="container mx-auto px-5 py-4 flex flex-col text-white">
+            {
+                isMobileMenuOpen && (
+                    <div ref={navRef} className={`lg:hidden flex flex-col justify-between ${Light ? "bg-white" : "bg-gradient" }  absolute top-20 left-0 right-0 overflow-hidden transition-all duration-300 ease-in-out z-20 ${isMobileMenuOpen ? "screen-minus-80 opacity-100 visible" : "h-0 opacity-0 invisible"}`}>
+                <nav className={`container  mx-auto px-5 py-4 flex flex-col ${Light ? "text-black" : "text-white"} divide-y-[1px]`}>
                     {Navbar.map((item, index) => (
-                        <Link key={index} href={item.link} className="hover:text-gray-300 py-2">
+                        <Link key={index} href={item.link} onClick={() => setIsMobileMenuOpen(false)} className="hover:text-gray-300 py-4 text-xl font-medium">
                             {item.name}
                         </Link>
                     ))}
-                    <div className="flex items-center justify-between pt-4 border-t border-gray-700">
-                        <Link href="/contact" className="bg-white text-black font-semibold py-2 px-4 rounded-full hover:bg-[var(--Blue-Color)] hover:text-white hover:border border transition-colors duration-300">
-                            Contact Us
-                        </Link>
-                    </div>
                 </nav>
+                <div className="flex items-center px-4 justify-between py-4 border-t ">
+                    <Link onClick={() => setIsMobileMenuOpen(false)} href="/contact" className={`w-full text-xl font-medium text-center   px-6 py-5  ${Light ? "rounded-lg  hover:bg-[#011633]  bg-[var(--Blue-Color)] text-white": "rounded-lg text-black bg-white  hover:bg-[var(--Blue-Color)] hover:text-white" } hover:border border transition-colors duration-300`}>
+                        Contact Us
+                    </Link>
+                </div>
             </div>
+                )
+            }
         </>
     );
 };
 
 export default Navbar;
-
-
-
-
